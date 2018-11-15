88b33e0c1b4e123bb6e557ffb2e2cbaae622cda2
Cise Midoglu <cise@simula.no>
2018-11-15 16:00:31 +0100

    MetadataCollector module initial commit.

---
+++ collector/src/main/java/com/bitmovin/analytics/MetadataCollector.java
@@ -0,0 +1,140 @@
+package com.bitmovin.analytics;
+
+import android.content.BroadcastReceiver;
+import android.location.LocationManager;
+import android.os.Build;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.DetailedState;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.telephony.CellLocation;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+
+
+/**
+ * A metadata collection module which reads network and hardware related information from the Android API.
+ */
+public class MetadataCollector {
+
+  private static final String TAG = "MetadataCollector";
+
+  /** Set to true if location information should be included */
+  public final static boolean INCLUDE_LOCATION = true;
+
+  /** Set to true if last signal information should be included */
+  public final static boolean INCLUDE_LAST_SIGNAL_ITEM = true;
+
+  /** Returned by getNetwork() if Wifi */
+  public static final int NETWORK_WIFI = 99;
+
+  /** Returned by getNetwork() if Ethernet */
+  public static final int NETWORK_ETHERNET = 106;
+
+  /** Returned by getNetwork() if Bluetooth */
+  public static final int NETWORK_BLUETOOTH = 107;
+
+  private static final String PLATTFORM_NAME = "Android";
+  private static final int ACCEPT_WIFI_RSSI_MIN = -113;
+  public static final int SIGNAL_TYPE_NO_SIGNAL = 0;
+  public static final int SIGNAL_TYPE_MOBILE = 1;
+  public static final int SIGNAL_TYPE_RSRP = 2;
+  public static final int SIGNAL_TYPE_WLAN = 3;
+
+  private ConnectivityManager connectivityManager = null;
+  private TelephonyManager telephonyManager = null;
+  private PhoneStateListener phoneStateListener = null;
+  private WifiManager wifiManager = null;
+
+  // Handlers and Receivers for phone and network state
+  private BroadcastReceiver networkReceiver;
+  private LocationManager locationManager = null;
+
+  private Location lastLocation;
+
+  private String OS_VERSION;
+  private String API_LEVEL;
+  private String DEVICE;
+  private String MODEL;
+  private String PRODUCT;
+
+  private String NETWORK_TYPE;
+  private String IS_ROAMING;
+
+  /**
+   * MetadataCollector
+   *
+   */
+
+  public MetadataCollector() {
+
+  }
+
+  public void setHardwareMetadata() {
+    OS_VERSION = android.os.Build.VERSION.RELEASE + "(" + android.os.Build.VERSION.INCREMENTAL + ")";
+    API_LEVEL = String.valueOf(android.os.Build.VERSION.SDK_INT);
+    DEVICE = android.os.Build.DEVICE;
+    MODEL = android.os.Build.MODEL;
+    PRODUCT = android.os.Build.PRODUCT;
+  }
+
+  public void setNetworkMetadata() {
+    NETWORK_TYPE = String.valueOf(getNetwork());
+    IS_ROAMING = String.valueOf(getRoaming());
+  }
+
+  public int getNetwork()
+  {
+    int result = TelephonyManager.NETWORK_TYPE_UNKNOWN;
+
+    if (connectivityManager != null)
+    {
+      final NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+      if (activeNetworkInfo != null)
+      {
+        final int type = activeNetworkInfo.getType();
+        switch (type)
+        {
+          case ConnectivityManager.TYPE_WIFI:
+            result = NETWORK_WIFI;
+            break;
+
+          case ConnectivityManager.TYPE_BLUETOOTH:
+            result = NETWORK_BLUETOOTH;
+            break;
+
+          case ConnectivityManager.TYPE_ETHERNET:
+            result = NETWORK_ETHERNET;
+            break;
+
+          case ConnectivityManager.TYPE_MOBILE:
+          case ConnectivityManager.TYPE_MOBILE_DUN:
+          case ConnectivityManager.TYPE_MOBILE_HIPRI:
+          case ConnectivityManager.TYPE_MOBILE_MMS:
+          case ConnectivityManager.TYPE_MOBILE_SUPL:
+            result = telephonyManager.getNetworkType();
+            break;
+        }
+      }
+    }
+    return result;
+  }
+
+  public boolean getRoaming()
+  {
+    boolean result = false;
+
+    if (connectivityManager != null)
+    {
+      final NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+      if (activeNetworkInfo != null)
+        result = activeNetworkInfo.isRoaming();
+    }
+    return result;
+  }
+}
